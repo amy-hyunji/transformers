239a556db8f78ff2eaeb43f1ad14f30fdd7d54f8
amy-hyunji <44370759+amy-hyunji@users.noreply.github.com>
2020-08-29 19:37:09 +0900

    add code for bert

--- examples/language-modeling/dataset.py
+++ examples/language-modeling/dataset.py
@@ -39,6 +39,7 @@ MDOEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
 
 """
 Loader function to solve MemoryError
+File format: .xz files inside a file
 """
 class SplitTextDataset(Dataset):
 
@@ -55,6 +56,80 @@ class SplitTextDataset(Dataset):
 		# ./ -> directory
 		cached_features_file = os.path.join(
 			"./", "cached_lm_{}_{}_{}".format(tokenizer.__class__.__name__, str(block_size), filename),
+		)
+		
+		xzFiles = os.listdir(self.file_path)
+		self.txtpath = "./owt_txt"
+		self.tensorpath = "./owt_tensor"	
+		if not os.path.exists(self.txtpath):
+			os.mkdir(self.txtpath)
+		print("Dumping txt file to owt_txt")
+		for i in tqdm(range(len(xzFiles))):
+			xzfile = xzFiles[i]
+			with tarfile.open(os.path.join(self.file_path, xzfile)) as f:
+				f.extractall(self.txtpath)
+
+		fileList = os.listdir(self.txtpath)
+		logger.info(f"{len(fileList)} number of file exists")
+		self.examples = list()
+		
+		if os.path.exists(cached_features_file) and not overwrite_cache:
+			logger.info(f"Loading features from {cached_features_file}")
+			self.examples = torch.load(cached_features_file)
+		else:
+			logger.info("Tokenizing the Dataset")
+			self._tokenize(fileList)
+			logger.info("Saving....")	
+			torch.save(self.examples, cached_features_file)
+			logger.info("Done Saving....")		
+		
+	def _tokenize(self, file_list):
+		for i in tqdm(range(len(file_list))):
+			_file = file_list[i]
+			fileList = list()
+			tokenized_text = list()
+			f = open(os.path.join(self.txtpath, _file), "r")
+			lines = f.readlines()
+			for line in lines:
+				text = line.rstrip('\n')
+				if (text == ""): continue
+				tokenized_text += self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(text))
+				if len(tokenized_text) > self.block_size:
+					self.examples.append(torch.tensor(self.tokenizer.build_inputs_with_special_tokens(tokenized_text[:self.block_size]), dtype=torch.long))
+					fileList.append(torch.tensor(self.tokenizer.build_inputs_with_special_tokens(tokenized_text[:self.block_size]), dtype=torch.long))
+					tokenized_text = tokenized_text[self.block_size:]
+			torch.save(fileList, os.path.join(self.tensorpath, _file+".txt"))
+			f.close()
+		return 
+	
+	def __len__(self):
+		return len(self.examples)
+
+	def __getitem__(self, i) -> torch.Tensor:
+		return torch.tensor(self.examples[i], dtype=torch.long)
+	
+"""
+Loader function to solve MemoryError
+File format: .xz files inside a file
+MultiProcess
+"""
+class MP_SplitTextDataset(Dataset):
+
+	def __init__(
+		self, tokenizer: PreTrainedTokenizer, file_path: str, block_size: int, overwrite_cache: False,
+	):
+		self.tokenizer = tokenizer
+		self.file_path = file_path
+		self.overwrite_cache = overwrite_cache
+		self.full_block_size = block_size
+		self.block_size = block_size
+		self.block_size -= tokenizer.num_special_tokens_to_add(pair=False)
+		directory, filename = os.path.split(file_path)
+		# ./ -> directory
+		self.tensorpath = "./owt_tensor"
+		os.makedirs(self.tensorpath, exist_ok=True)
+		self.cached_features_file = os.path.join(
+			self.tensorpath, "cached_lm_{}_{}_{}".format(tokenizer.__class__.__name__, str(block_size), filename),
 		)
 		
 		num_cores = int(multiprocessing.cpu_count() / 2)
@@ -81,23 +156,21 @@ class SplitTextDataset(Dataset):
 				split_file.append(fileList[file_per_core*i:file_per_core*(i+1)])
 		self.examples = list()
 		
-		with FileLock(cached_features_file+".lock"):
-			if os.path.exists(cached_features_file) and not overwrite_cache:
-				logger.info(f"Loading features from {cached_features_file}")
-				self.examples = torch.load(cached_features_file)
+		with FileLock(self.cached_features_file+".lock"):
+			if os.path.exists(self.cached_features_file) and not overwrite_cache:
+				logger.info(f"Loading features from {self.cached_features_file}")
+				self.examples = torch.load(self.cached_features_file)
 			else:
 				pool = ProcessingPool(nodes=num_cores)
 				temp = pool.map(self._tokenize, split_file)
-				for _list in temp:
-					self.examples += _list
-				logger.info("Saving....")	
-				torch.save(self.examples, cached_features_file)
-				logger.info("Done Saving....")		
+				logger.info("Done Saving all!!")
+				sys.exit()
 		
 	def _tokenize(self, file_list):
 		defList = list()
+		p_feature_file = self.cached_features_file + f"_{os.getpid()}"
 		for i in range(len(file_list)):
-			if i%30 == 0:
+			if i%100000 == 0:
 				print(f"pid: {os.getpid()} is working on {i}/{len(file_list)}")
 			_file = file_list[i]
 			tokenized_text = list()
@@ -110,6 +183,12 @@ class SplitTextDataset(Dataset):
 				if len(tokenized_text) > self.block_size:
 					defList.append(torch.tensor(self.tokenizer.build_inputs_with_special_tokens(tokenized_text[:self.block_size]), dtype=torch.long))
 					tokenized_text = tokenized_text[self.block_size:]
+			f.close()
+		
+#os.system("rm {os.path.join(self.txtpath, _file)}")
+		print(f"Saving pid {os.getpid()}. Dumping list of length {len(defList)}")
+		torch.save(defList, p_feature_file)
+		print(f"Done saving pid {os.getpid()}")
 		return defList 
 	
 	def __len__(self):
@@ -117,7 +196,29 @@ class SplitTextDataset(Dataset):
 
 	def __getitem__(self, i) -> torch.Tensor:
 		return torch.tensor(self.examples[i], dtype=torch.long)
-					
+	
+class BertTextDataset(Dataset):
+    """
+    This will be superseded by a framework-agnostic approach
+    soon.
+    """
+
+    def __init__(
+        self, tokenizer: PreTrainedTokenizer, file_path: str, block_size: int, overwrite_cache=False,
+    ):
+          start = time.time()
+          self.examples = torch.load(file_path)
+          logger.info(
+               "Loading features from cached file %s [took %.3f s]", file_path, time.time() - start
+           )
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i) -> torch.Tensor:
+        return torch.tensor(self.examples[i], dtype=torch.long)
+
+				
 class TextDataset(Dataset):
     """
     This will be superseded by a framework-agnostic approach
@@ -138,39 +239,41 @@ class TextDataset(Dataset):
 
         # Make sure only the first process in distributed training processes the dataset,
         # and the others will use the cache.
-        lock_path = cached_features_file + ".lock"
-        with FileLock(lock_path):
 
-            if os.path.exists(cached_features_file) and not overwrite_cache:
-                start = time.time()
-                self.examples = torch.load(cached_features_file)
-                logger.info(
-                    "Loading features from cached file %s [took %.3f s]", cached_features_file, time.time() - start
-                )
+        if os.path.exists(cached_features_file) and not overwrite_cache:
+             start = time.time()
+             self.examples = torch.load(cached_features_file)
+             logger.info(
+                  "Loading features from cached file %s [took %.3f s]", cached_features_file, time.time() - start
+             )
 
-            else:
-                logger.info("Creating features from dataset file at %s", directory)
-                self.examples = list()
-                with open(file_path, encoding="utf-8") as f:
-                    tokenized_text = list()
-                    lines = f.readlines()
-                    for i in tqdm(range(len(lines))):
-                        line = lines[i]
-                        text = line.rstrip('\n')
-                        tokenized_text += tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
-                        if len(tokenized_text)>block_size:
-                            self.examples.append(torch.tensor(tokenizer.build_inputs_with_special_tokens(tokenized_text[:block_size]), dtype=torch.long))
-                            tokenized_text = tokenized_text[block_size:]
+
+        else:
+#           lock_path = cached_features_file + ".lock"
+#           with FileLock(lock_path):
+            logger.info("Creating features from dataset file at %s", directory)
+            self.examples = list()
+            logger.info(f"Opening: {file_path}")
+            with open(file_path, encoding="utf-8") as f:
+                 tokenized_text = list()
+                 lines = f.readlines()
+                 for i in tqdm(range(len(lines))):
+                     line = lines[i]
+                     text = line.rstrip('\n')
+                     tokenized_text += tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
+                     if len(tokenized_text)>block_size:
+                        self.examples.append(torch.tensor(tokenizer.build_inputs_with_special_tokens(tokenized_text[:block_size]), dtype=torch.long))
+                        tokenized_text = tokenized_text[block_size:]
 
                 # Note that we are losing the last truncated example here for the sake of simplicity (no padding)
                 # If your dataset is small, first you should loook for a bigger one :-) and second you
                 # can change this behavior by adding (model specific) padding.
 
-                start = time.time()
-                torch.save(self.examples, cached_features_file)
-                logger.info(
-                    "Saving features into cached file %s [took %.3f s]", cached_features_file, time.time() - start
-                )
+            start = time.time()
+            torch.save(self.examples, cached_features_file)
+            logger.info(
+               "Saving features into cached file %s [took %.3f s]", cached_features_file, time.time() - start
+            )
 
     def __len__(self):
         return len(self.examples)
